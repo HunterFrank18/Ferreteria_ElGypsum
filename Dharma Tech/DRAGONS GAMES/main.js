50620e13154c03608efdb03547c99d8ec4e0662b
Mr Castle <134983817+HunterFrank18@users.noreply.github.com>
2026-04-12 14:21:52 -0600

    Delete Dharma Tech directory

--- Dharma Tech/DRAGONS GAMES/main.js
+++
@@ -1,8 +0,0 @@
-let listElements = document.querySelectorAll('.list__item--click');
-
-listElements.forEach(listElement => {
-    listElements.addEventListener('click',()=>)
-    
-
-    listElement.classList.toggle
-})
--- Dharma Tech/eo.c
+++
@@ -1,8 +0,0 @@
-int main()
-{
-    printf( "Hola mundo." );
-
-    getch(); /* Pausa */
-
-    return 0;
-}
