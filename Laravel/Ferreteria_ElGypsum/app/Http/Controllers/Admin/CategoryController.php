e0946004d69ee9b707daf40cfab3f08de293bb71
HunterFrank18 <fjquezada77@gmail.com>
2026-09-16 20:54:59 -0600

    renombrando archivos

--- Laravel/Ferreteria_ElGypsum/app/Http/Controllers/Admin/CategoryController.php
+++ Laravel/Ferreteria_ElGypsum/app/Http/Controllers/Admin/CategoryController.php
@@ -1,70 +1,70 @@
 <?php
-
+ 
 namespace App\Http\Controllers\Admin;
-
+ 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
-
+ 
 class CategoryController extends Controller
 {
-
+ 
     public function index()
     {
         $categories = Category::all();
-        return view('Admin.categories.index', compact('categories'));
+        return view('admin.categories.index', compact('categories'));
     }
-
+ 
     public function create()
     {
-        return view('Admin.categories.create');
+        return view('admin.categories.create');
     }
-
+ 
     public function store(Request $request)
     {
         $request->validate([
             'name' => 'required'
         ]);
-
+ 
         Category::create([
             'name' => $request->name,
             'slug' => Str::slug($request->name)
         ]);
-
+ 
         return redirect()->route('admin.categories.index')
             ->with('success','Categoría creada correctamente');
     }
-
+ 
     public function edit(string $id)
     {
         $category = Category::findOrFail($id);
-        return view('Admin.categories.edit', compact('category'));
+        return view('admin.categories.edit', compact('category'));
     }
-
+ 
     public function update(Request $request, string $id)
     {
         $request->validate([
             'name' => 'required'
         ]);
-
+ 
         $category = Category::findOrFail($id);
-
+ 
         $category->update([
             'name' => $request->name,
             'slug' => Str::slug($request->name)
         ]);
-
+ 
         return redirect()->route('admin.categories.index')
             ->with('success','Categoría actualizada');
     }
-
+ 
     public function destroy(string $id)
     {
         $category = Category::findOrFail($id);
-
+ 
         $category->delete();
-
+ 
         return redirect()->route('admin.categories.index')
             ->with('success','Categoría eliminada');
     }
